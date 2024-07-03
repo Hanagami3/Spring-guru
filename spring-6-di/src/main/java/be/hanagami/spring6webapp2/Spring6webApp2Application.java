d02256307002c6fe0d38a20ce1a0dbf94ae1e67a
Hanagami3 <hanaga3mi@gmail.com>
2024-07-03 16:49:27 +0200

    2d project start

---
+++ spring-6-di/src/main/java/be/hanagami/spring6webapp2/Spring6webApp2Application.java
@@ -0,0 +1,13 @@
+package be.hanagami.spring6webapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6webApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6webApp2Application.class, args);
+    }
+
+}
---
+++ spring-6-di/src/test/java/be/hanagami/spring6webapp2/Spring6webApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hanagami.spring6webapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6webApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
